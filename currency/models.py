8d521a0e950fb55179abfdbe2aca3f2473c1cc3f
Сергей <goroxov.99@mail.ru>
2025-08-05 12:59:37 +0300

    add validator in model Currency for field "rate", create migration

--- currency/models.py
+++ currency/models.py
@@ -1,10 +1,11 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Currency(models.Model):
     name = models.CharField(max_length=100, verbose_name="Имя валюты")
     code = models.CharField(max_length=3, verbose_name="Код валюты")
-    rate = models.DecimalField(max_digits=4, decimal_places=4,
+    rate = models.DecimalField(max_digits=4, decimal_places=4, validators=[MinValueValidator(0)],
                                verbose_name="Курс валюты к рублю")  # max 4 знака после запятой
 
     def __str__(self):
