714d1b35fde9d61137608f171b3953c94c210aac
Сергей <goroxov.99@mail.ru>
2025-08-05 13:27:15 +0300

    add paginations for CurrencyListAPIView

--- currency/views.py
+++ currency/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from currency.models import Currency
+from currency.paginators import CurrencyPagination
 from currency.serializers import CurrencySerializers
 
 
 class CurrencyListAPIView(ListAPIView):
     queryset = Currency.objects.all().order_by('name')
     serializer_class = CurrencySerializers
+    pagination_class = CurrencyPagination
+
 
 
 class CurrencyRetrieveAPIView(RetrieveAPIView):
