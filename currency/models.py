006d5789d02b49aaeda0350c6188ab6a4edad630
Сергей <goroxov.99@mail.ru>
2025-08-05 12:33:54 +0300

    add model Currency, create migration

--- currency/models.py
+++ currency/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Имя валюты")
+    code = models.CharField(max_length=3, verbose_name="Код валюты")
+    rate = models.DecimalField(max_digits=4, decimal_places=4,
+                               verbose_name="Курс валюты к рублю")  # max 4 знака после запятой
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Валюта"
+        verbose_name_plural = "Валюты"
