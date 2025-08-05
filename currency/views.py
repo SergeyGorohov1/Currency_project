462ad50611071fa335938d0eeb79c54d86029963
Сергей <goroxov.99@mail.ru>
2025-08-05 13:11:32 +0300

    add views: List and Retrieve

--- currency/views.py
+++ currency/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from currency.models import Currency
 
-# Create your views here.
+
+class CurrencyListAPIView(ListAPIView):
+    queryset = Currency.objects.all()
+
+
+class CurrencyRetrieveAPIView(RetrieveAPIView):
+    queryset = Currency.objects.all()
+
